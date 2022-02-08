aa45a16e63849a3a6427af0de83c474170a60178
a2uuz <aziznageeye9@gmail.com>
2022-02-08 21:26:55 +0300

    hide or show list items

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let botn = document.querySelector('.btn-main');
 let headingOne = document.getElementById('headline');
-
 botn.addEventListener(`click`, () =>{
 
   // Local Variables
@@ -15,4 +14,31 @@ content in the DOM || and .value reads the value
 typed into the field */  
 headingOne.innerHTML = input.value;
   input.value = '';
+
+// update button bgcolor and border when heading updates
+botn.style.borderBottom = '5px solid deeppink'
+botn.style.backgroundColor = 'mediumvioletred' 
+
+
 })
+
+// Hide and Show List 
+const toggleList = document.querySelector('.btn-toggle');
+// Event Listener
+toggleList.addEventListener('click', () =>{
+
+// The Block of Elements that's gonna be effected when clicked
+const showList = document.querySelector('.list-container');
+
+/* if the list is set to display: none show it by turning it back 
+  to it is default display: block or else hide it by display:none 
+*/
+if (showList.style.display === 'none') {
+  toggleList.innerHTML = 'Hide List'
+  showList.removeAttribute('style');
+} else {
+  toggleList.innerHTML = 'Show List'
+  showList.style.display = 'none';
+}
+
+});
